f4a9d273fce2633802af924fe485810aac5649b1
scottw0173 <swarner5937@gmail.com>
2026-08-02 00:22:51 -0700

    feat: add bootstrap script
    add a script that will deploy to current bicep configuration, then
    build and push a docker image to ACR, then update the bicep config to
    point at the docker image

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#run this script to deploy the bicep configuration
+#then build and push the docker image tagged to the latest
+#SHA from github and then update the bicep to point at the image
+
+set -euo pipefail
+
+RG="${RG:-jobfinder-rg}"
+ADMIN_OID=$(az ad signed-in-user show --query id -o tsv)
+ADMIN_UPN=$(az ad signed-in-user show --query userPrincipalName -o tsv)
+#Deploying the bicep initially to ACR. 
+az deployment group create -g "$RG" -f ./infra/main.bicep -p ./infra/main.bicepparam -p postgresAdminObjectId="$ADMIN_OID" -p postgresAdminPrincipalName="$ADMIN_UPN"
+
+ACR_LOGIN=$(az acr list -g "$RG" --query "[0].loginServer" -o tsv)
+ACR=$(az acr list -g "$RG" --query "[0].name" -o tsv)
+TAG=$(git rev-parse --short HEAD) #currently set to the shortened SHA of the last git commit. Probably need to adjust in final product
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+az acr login -n "$ACR"
+docker build --platform linux/amd64 -t "$ACR_LOGIN/jobfinder:$TAG" "$REPO_ROOT"
+docker push "$ACR_LOGIN/jobfinder:$TAG"
+
+JOB_NAME=$(az containerapp job list -g "$RG" --query "[0].name" -o tsv)
+
+az containerapp job update -n "$JOB_NAME" -g "$RG" --image "$ACR_LOGIN/jobfinder:$TAG"
